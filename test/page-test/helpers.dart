024c33cbedd35cbcd7f2dde87fada40f19de8773
Manuel David Guzman Chavez <davguzm20@users.noreply.github.com>
2026-05-27 18:30:03 -0500

    test: add GoRouter support to page test helper

--- test/page-test/helpers.dart
+++ test/page-test/helpers.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 Future<void> pumpPage(
@@ -11,6 +12,26 @@ Future<void> pumpPage(
     MultiProvider(
       providers: providers,
       child: MaterialApp(home: page),
+    ),
+  );
+}
+
+Future<void> pumpPageWithRouter(
+  WidgetTester tester, {
+  required String location,
+  required Widget page,
+  required List<Provider> providers,
+}) async {
+  final router = GoRouter(
+    initialLocation: location,
+    routes: [
+      GoRoute(path: location, builder: (_, __) => page),
+    ],
+  );
+  await tester.pumpWidget(
+    MultiProvider(
+      providers: providers,
+      child: MaterialApp.router(routerConfig: router),
     ),
   );
 }
