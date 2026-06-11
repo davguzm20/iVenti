e7ab7d0a1f37fae8934c8711caa494530f09ebd0
Manuel David Guzman Chavez <davguzm20@users.noreply.github.com>
2026-06-10 20:43:20 -0500

    feat: device_registered flag to skip welcome on known devices

--- lib/AppRoutes.dart
+++ lib/AppRoutes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:iventi/shared/di/ServiceLocator.dart';
 
@@ -47,12 +48,11 @@ class AppRoutes {
       final path = state.matchedLocation;
 
       if (path == '/welcome') {
-        try {
-          await ServiceLocator.authController.obtenerUsuarioRegistrado();
+        final prefs = await SharedPreferences.getInstance();
+        if (prefs.getBool('device_registered') == true) {
           return '/login';
-        } catch (_) {
-          return null;
         }
+        return null;
       }
 
       if (path == '/login') {
